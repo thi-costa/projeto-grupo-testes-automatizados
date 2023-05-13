031e3bca70bd46bf3b503afcb2b3d0fa639b0f51
thi-costa <41833533+thi-costa@users.noreply.github.com>
2023-05-12 22:27:24 -0300

    chore: update iml and optimize some imports

--- demo/src/test/java/br/com/controller/CarrinhoControllerTest.java
+++ demo/src/test/java/br/com/controller/CarrinhoControllerTest.java
@@ -1,24 +1,21 @@
 package br.com.controller;
 
-import br.com.domain.Produto;
 import br.com.model.entity.ProdutoEntity;
 import br.com.repository.CarrinhoRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.servlet.ServletException;
-import org.aspectj.lang.annotation.Before;
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.annotation.DirtiesContext;
-import org.springframework.test.context.event.annotation.BeforeTestClass;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-import java.util.HashMap;
 import java.util.Optional;
 
 @SpringBootTest
